1eb675a3a686f0ff000b08f05b0a19669c87b8bc
SilisK <kleemoffdeveloper@gmail.com>
2024-06-06 14:03:45 -0400

    added CRUD methods to location service

--- Services/LocationService.cs
+++ Services/LocationService.cs
@@ -9,7 +9,33 @@ public static class LocationService
     {
         Locations = new List<Location>
         {
-            new Location {}
+            new Location { City = "Brooklyn", State = "New York", Forecast = WeatherForecast.CreateNewForecast() },
+            new Location { City = "Los Angeles", State = "California", Forecast = WeatherForecast.CreateNewForecast() },
         };
+    }
+
+    public static List<Location> GetAll() => Locations;
+
+    public static Location? Get(Guid id) => Locations.FirstOrDefault(loc => loc.Id == id);
+
+    public static void Add(Location location)
+    {
+        Locations.Add(location);
+    }
+
+    public static void Delete(Guid id)
+    {
+        var location = Get(id);
+        if (location is null) return;
+
+        Locations.Remove(location);
+    }
+
+    public static void Update(Location location)
+    {
+        var index = Locations.FindIndex(loc => loc.Id == location.Id);
+        if (index == -1) return;
+
+        Locations[index] = location;
     }
 }
